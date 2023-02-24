b797a6c0ea1795080ae25d85d7bb8476777d1215
Viljan Gerbrand Emanuel Van Raaij <vgraaij@stud.ntnu.no>
2023-02-24 09:16:21 +0100

    feat: change to nextui navbar

--- src/app/Header.js
+++ src/app/Header.js
@@ -5,9 +5,15 @@ import pb from './(lib)/pocketbase';
 import styles from './Header.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Dropdown, Navbar, User, Text } from '@nextui-org/react';
+import useLogout from './(hooks)/useLogout';
 
 export default function Header() {
+  const logout = useLogout();
   const [username, setUsername] = useState();
+  const [avatar, setAvatar] = useState();
+
+  const [activePage, setActivePage] = useState(0);
 
   useEffect(() => {
     setUsername(pb.authStore.model?.username);
@@ -17,18 +23,38 @@ export default function Header() {
     setUsername(model?.username);
   });
 
+  try {
+    pb.collection('users')
+      .getOne(pb.authStore.model.id)
+      .then((data) => {
+        setAvatar(pb.getFileUrl(data, data.avatar));
+      });
+  } catch (error) {}
+
   return (
-    <nav className={styles.navBar}>
-      <Link href="/">
-        <Image
-          src="/Lendables_light.png"
-          height={52}
-          width={180}
-          alt={'logo'}
-        />
-      </Link>
-      <ul>
-        <li>
+    <Navbar className={styles.navBar}>
+      <Navbar.Brand>
+        <Link href="/">
+          <Image
+            onClick={(e) => {
+              setActivePage(0);
+            }}
+            src="/Lendables_light.png"
+            height={52}
+            width={180}
+            alt={'logo'}
+          />
+        </Link>
+      </Navbar.Brand>
+
+      <Navbar.Content>
+        <Navbar.Link
+          isActive={activePage === 1}
+          activeColor={'success'}
+          onPress={(e) => {
+            setActivePage(1);
+          }}
+        >
           <Link href="/posts">
             <link
               rel="stylesheet"
@@ -37,10 +63,17 @@ export default function Header() {
             <span className={`${styles.icon} material-symbols-outlined`}>
               search
             </span>
-            Søk
+            Search
           </Link>
-        </li>
-        <li>
+        </Navbar.Link>
+
+        <Navbar.Link
+          isActive={activePage === 2}
+          activeColor={'success'}
+          onPress={(e) => {
+            setActivePage(2);
+          }}
+        >
           <Link href="/createad">
             <link
               rel="stylesheet"
@@ -49,22 +82,49 @@ export default function Header() {
             <span className={`${styles.icon} material-symbols-outlined`}>
               add_circle
             </span>
-            Ny annonse
+            New post
           </Link>
-        </li>
-        <li>
-          <Link href="/login">
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,500,1,0"
-            />
-            <span className={`${styles.icon} material-symbols-outlined`}>
-              account_circle
-            </span>
-            {username ?? 'Log in'}
-          </Link>
-        </li>
-      </ul>
-    </nav>
+        </Navbar.Link>
+        {username ? (
+          <Dropdown>
+            <Navbar.Item>
+              <Dropdown.Trigger>
+                <User name={username} size="sm" src={avatar} />
+              </Dropdown.Trigger>
+            </Navbar.Item>
+            <Dropdown.Menu
+              onAction={(key) => {
+                key == 'logout' && logout();
+              }}
+            >
+              <Dropdown.Item key="account" css={{ height: '$18' }}>
+                <Text b color="inherit" css={{ d: 'flex', margin: '$2 $0' }}>
+                  Signed in as
+                </Text>
+                <Text b color="inherit" css={{ d: 'flex', margin: '$2 $0' }}>
+                  {pb.authStore.model.email}
+                </Text>
+              </Dropdown.Item>
+              <Dropdown.Item key="posts" withDivider>
+                My posts
+              </Dropdown.Item>
+              <Dropdown.Item key="logout" withDivider color={'error'}>
+                Log out
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        ) : (
+          <Navbar.Link
+            onPress={(e) => {
+              setActivePage(3);
+            }}
+          >
+            <Link href="/login">
+              <User name={'Log in'} size="sm" />
+            </Link>
+          </Navbar.Link>
+        )}
+      </Navbar.Content>
+    </Navbar>
   );
 }
