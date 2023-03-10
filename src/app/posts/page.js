115787cdc7bac99ca85433317cb8357d66c2f1dc
runarli <runarli@stud.ntnu.no>
2023-03-11 00:06:17 +0100

    Fix search functionality for posts
    Earlier the posts you searched for would not be updated on the page, but now they do.
    
    Relates #16

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,17 +1,8 @@
 import Posts from './Posts';
-import Searchbar from './Searchbar';
 
 export default function PostsPage() {
   return (
     <>
-      <Searchbar />
-      <hr
-        style={{
-          color: 'lightgrey',
-          height: '1px',
-          width: '1200px',
-        }}
-      />
       <Posts />
     </>
   );
